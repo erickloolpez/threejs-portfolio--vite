116ba3e2aae8380361987513b5efbc1f15557dbc
erickloolpez <erickloopeze@hotmail.com>
2025-08-01 20:49:24 -0500

    feat(hero): Adding the hero section for our porfolio withouth threejs image yet.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import { useState } from 'react'
+import Hero from './sections/Hero'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-  <main className="bg-blue-400">
-      <div>
-        <h1>Welcome to my portfolio</h1>
-      </div>
+  <main>
+      <Hero />
     </main>
   )
 }
