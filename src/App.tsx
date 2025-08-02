9c34847f81175c08dfe5114247c3bcf9e07932c2
erickloolpez <erickloopeze@hotmail.com>
2025-08-01 20:06:39 -0500

    feat(hero): Setting up the project to start coding.

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+  <main className="bg-blue-400">
+      <div>
+        <h1>Welcome to my portfolio</h1>
+      </div>
+    </main>
+  )
+}
+
+export default App
